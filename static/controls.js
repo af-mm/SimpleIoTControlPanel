bce868ce4c2b1b2ba2d39ea69c2d173fd02256e2
BuildTools <unconfigured@null.spigotmc.org>
2019-11-28 20:46:47 +0300

    controls

--- static/controls.js
+++ static/controls.js
@@ -1,6 +1,7 @@
 function activateAction(globalActionId){
 	var e = document.getElementById("a" + globalActionId);
-	var delay = 5000;
+	var children = e.parentNode.childNodes;
+	var delay = 250;
 	
 	fetch("/api/activateAction?globalActionId=" + globalActionId)
 		.then(function(d){
@@ -9,14 +10,23 @@ function activateAction(globalActionId){
 		.then(function(d){
 			if(d.result == 0){
 				e.classList.toggle("failure", 1);
+				for(var i = 0; i < children.length; ++i)
+					children[i].disabled = 1;
+				
 				setTimeout(function(){
 					e.classList.toggle("failure", 0);
-				}, delay);
-				throw new Error("d.result != 1");
+					for(var i = 0; i < children.length; ++i)
+						children[i].disabled = 0;
+				}, delay * 10);
 			}else{
 				e.classList.toggle("success", 1);
+				for(var i = 0; i < children.length; ++i)
+					children[i].disabled = 1;
+				
 				setTimeout(function(){
 					e.classList.toggle("success", 0);
+					for(var i = 0; i < children.length; ++i)
+						children[i].disabled = 0;
 				}, delay);
 			}
 				
